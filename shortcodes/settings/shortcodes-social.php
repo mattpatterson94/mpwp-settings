04de5bbdcffe11e950862e71c7a95a2e6a17c324
Matt Patterson <matt.patterson94@gmail.com>
2017-08-30 21:09:45 +1000

    Add ability to add class to social icon list

--- shortcodes/settings/shortcodes-social.php
+++ shortcodes/settings/shortcodes-social.php
@@ -10,23 +10,25 @@ function shortcode_social_url($atts) {
   return get_field($args['product'] . '_url', 'option');
 }
 
-//[social-icons]
+//[social-icons class="social-icons"]
 function shortcode_social_icons() {
+  $args = shortcode_atts(array('class' => 'social-icons',), $atts);
   $available_social_medias = ['facebook', 'twitter', 'instagram', 'pinterest', 'youtube-play', 'envelope-o'];
 
   $html = "";
   $social_media_html = "";
+  $class = $args['class'];
 
   foreach($available_social_medias as $sm) {
     $sm_url = get_field($sm . '_url', 'option');
     if($sm == 'envelope-o') $sm_url = "mailto:" . $sm_url;
     if($sm_url) {
-      $social_media_html .= "<li><a href='$sm_url'><i class='fa fa-$sm'></i></a></li>";
+      $social_media_html .= "<li class='".$class."__icon'><a href='$sm_url'><i class='fa fa-$sm'></i></a></li>";
     }
   }
 
   if(!empty($social_media_html)) {
-    $html = "<ul class='social-icons'>";
+    $html = "<ul class='$class'>";
     $html .= $social_media_html;
     $html .= "</ul>";
   }
